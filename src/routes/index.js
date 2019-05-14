ca2695ad7ae8f43c2f448b79e147ce4bfe025e54
Santhosh Vasabhaktula <santhosh@ilimi.in>
2019-05-14 19:56:32 +0530

    Page assemble test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express'),
   router = express.Router(),
-  telemetryService = require('../service/telemetry-service');
+  telemetryService = require('../service/telemetry-service'),
+  pageService = require('../service/page-service');
 
 router.post('/v1/telemetry', (req, res) => telemetryService.dispatch(req, res));
+router.post('/v1/page/assemble', (req, res) => pageService.assemble(req, res));
 
 router.get('/health', (req, res) => telemetryService.health(req, res));
 
